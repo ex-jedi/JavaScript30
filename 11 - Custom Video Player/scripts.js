da786ba2f0946218fd5b95c3d869dc4f81f37058
Mark Phoenix <mark@pixelsmiths.co.uk>
2023-03-06 10:31:11 +0000

    Adding skip

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -19,9 +19,18 @@ function updateButton() {
   console.log(video.paused);
 }
 
+
+function skip() {
+
+  video.currentTime += parseFloat(this.dataset.skip);
+  console.log(video.currentTime);
+  console.log(this.dataset.skip);
+}
+
 // Hook up event listeners
 
 video.addEventListener('click', togglePlay);
 video.addEventListener('play', updateButton);
 video.addEventListener('pause', updateButton);
 toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip))
